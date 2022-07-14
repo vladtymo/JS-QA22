eca04014705504fea4b995b76c90c02ddf0b6643
Vlad Tymoshchuk <59306514+vladtymo@users.noreply.github.com>
2022-07-14 19:28:32 +0300

    do while, for cycles examples.

--- 01_Base/cycles.js
+++ 01_Base/cycles.js
@@ -33,3 +33,58 @@ while (mark < 1 || mark > 5) {
 }
 
 console.log("Great! Your mark is " + mark);
+
+// -=-=-=-=-=-=-=-=- do while -=-=-=-=-=-=-=-=-
+// do {
+//     code... if condition = true    
+// } while (condition);
+
+// enter student's mark while value out of the range 1...5
+do {
+    mark = +prompt("Enter student's mark in the range 1...5:");
+
+} while (mark < 1 || mark > 5);
+
+console.log("Great! Your mark is " + mark);
+
+// enter numbers until 0
+let num = 0;
+let summ = 0;
+do {
+    num = +prompt("Enter a number:");
+    summ += num;
+
+} while (num != 0);
+
+console.log("Summ = " + summ);
+
+// -=-=-=-=-=-=-=-=- for -=-=-=-=-=-=-=-=-
+// for(initialize variables; condition; expression) { code... }
+
+for (let i = 0; i < 10; ++i) {
+    console.log(`Iteration [${i}]`);
+}
+// the same with 'while' cycle
+// let i = 0;
+// while (i < 10) {
+//     console.log(`Iteration [${i}]`);
+//     ++i;
+// }
+
+for (let a = 1, b = 10; a < b; ++a, --b) {
+    console.log(`a: [${a}] - b: [${b}]`);
+}
+
+// show factorial of the user's number
+// !5 = 1 * 2 * 3 * 4 * 5 = 120
+num = +prompt("Enter a number to get factorial:");
+let result = 1;
+
+for (let i = 2; i <= num; ++i) {
+    result *= i;
+}
+
+console.log(`!${num} = ${result}`);
+
+// -=-=-=-=-=-=-=-=- for of -=-=-=-=-=-=-=-=-
+// -=-=-=-=-=-=-=-=- for in -=-=-=-=-=-=-=-=-
